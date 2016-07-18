1c019758ace7dd29b4c740e5fb09a1bfcf38a7c6
MaxPlugins-Trid <TridiumX1@gmail.com>
2016-07-18 01:08:41 -0400

    Fixed PowerManager

--- src/com/tridiumx/maxitems/Powers/PowerManager.java
+++ src/com/tridiumx/maxitems/Powers/PowerManager.java
@@ -9,12 +9,14 @@ import org.bukkit.plugin.Plugin;
  * Created by TridiumX on 7/17/2016.
  */
 public class PowerManager {
+    //Variable for powers
     MaxPower lifeSteal;
     MaxPower blink;
 
 
 
     public void loadPowers(Plugin plugin){
+        //Init powers
         lifeSteal = new LifeSteal();
         lifeSteal.onEnable(plugin, new CoolDownGenerator());
         blink = new Blink();
