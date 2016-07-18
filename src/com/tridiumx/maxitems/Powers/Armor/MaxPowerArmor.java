f98064d16408413df6ad2a7cf97342a3c3436718
MaxPlugins-Trid <TridiumX1@gmail.com>
2016-07-18 19:41:55 -0400

    Added new power type (Tool)

---
+++ src/com/tridiumx/maxitems/Powers/Armor/MaxPowerArmor.java
@@ -0,0 +1,14 @@
+package com.tridiumx.maxitems.Powers.Armor;
+
+import com.tridiumx.maxitems.Powers.MaxPower;
+
+/**
+ * Created by TridiumX on 7/18/2016.
+ */
+public abstract class MaxPowerArmor extends MaxPower {
+
+
+//TODO MAKE EVENT TO CHECK ARMOR
+
+
+}
